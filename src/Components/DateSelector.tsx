0935c7e883405ac2447e2341c6bf8d9f3c4450e8
hi_go <me@clutte.red>
2022-08-28 12:22:20 +0900

    Implement `DateSelector`

--- src/Components/DateSelector.tsx
+++ src/Components/DateSelector.tsx
@@ -3,11 +3,10 @@ import { DateInput2 } from "@blueprintjs/datetime2";
 import { format, parse } from "date-fns";
 import React from "react";
 
-export class DateSelector extends React.Component {
+export class DateSelector extends React.Component<any, { dateValue: Date }> {
     private static dateFnsFormat = 'yyyy-MM-dd';
     private static formatDate = (date: Date) => format(date, DateSelector.dateFnsFormat);
     private static parseDate = (str: string) => parse(str, DateSelector.dateFnsFormat, new Date());
-    //private readonly dateValue = format(new Date(), DateSelector.dateFnsFormat);
 
     constructor(props: any) {
         super(props);
@@ -31,36 +30,47 @@ export class DateSelector extends React.Component {
                     shortcuts
                     defaultValue={this.getTodayStr()}
                     reverseMonthAndYearMenus
+                    value={DateSelector.formatDate(this.state.dateValue)}
                 />
-                <AnchorButton icon='chevron-left' minimal onClick={this.setPreviousDay}/>
-                <AnchorButton icon='time' minimal onClick={this.setToday}/>
-                <AnchorButton icon='chevron-right' minimal />
+                <AnchorButton icon='chevron-left' minimal onClick={this.setPreviousDay} />
+                <AnchorButton icon='time' minimal onClick={this.setToday} />
+                <AnchorButton icon='chevron-right' minimal onClick={this.setNextDay} />
             </NavbarGroup>
         );
     }
 
-    private handleChange(newDateStr: string|null){
+    private handleChange(newDateStr: string | null) {
         this.setState({
-            dateValue: DateSelector.parseDate(newDateStr??this.getTodayStr())
+            dateValue: DateSelector.parseDate(newDateStr ?? this.getTodayStr())
         });
     }
 
     private readonly mSecPerDay = 1000 * 60 * 60 * 24;
-    private setNextDay(currentDate: Date){
-        //this.handleChange(this.state)
-    }
-
-    private setPreviousDay(){
+    private setNextDay() {
         this.setState(state => {
-            //dateValue: state.dateValue.getTime() - this.mSecPerDay
+            return {
+                dateValue: new Date(state.dateValue.getTime() + this.mSecPerDay)
+            }
         });
     }
 
-    private setToday(){
-        this.handleChange(this.getTodayStr());
+    private setPreviousDay() {
+        this.setState(state => {
+            return {
+                dateValue: new Date(state.dateValue.getTime() - this.mSecPerDay)
+            }
+        });
     }
 
-    private getTodayStr(){
+    private setToday() {
+        this.setState(state => {
+            return {
+                dateValue: new Date()
+            }
+        });
+    }
+
+    private getTodayStr() {
         return format(new Date(), DateSelector.dateFnsFormat);
     }
 
