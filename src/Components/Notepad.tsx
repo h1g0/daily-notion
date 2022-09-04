0524e20699ef4d213456118523a33e275b0530bf
hi_go <me@clutte.red>
2022-09-04 20:58:12 +0900

    Refactor

--- src/Components/Notepad.tsx
+++ src/Components/Notepad.tsx
@@ -2,7 +2,7 @@ import { TextArea } from "@blueprintjs/core";
 import React from "react";
 import { NotionHandler } from "../Data/NotionHandler";
 
-export class Notepad extends React.Component<{dateStr: string}, {blockId: string, text: string }> {
+export class Notepad extends React.Component<{ dateStr: string }, { blockId: string, text: string }> {
     constructor(props: any) {
         super(props);
         this.state = {
@@ -13,28 +13,28 @@ export class Notepad extends React.Component<{dateStr: string}, {blockId: string
 
     render() {
         return (
-                <TextArea
-                    fill
-                    placeholder='some text here...'
-                    className='mainText'
-                    minLength={20}
-                    value={this.state.text}
-                />
+            <TextArea
+                fill
+                placeholder='some text here...'
+                className='mainText'
+                minLength={20}
+                value={this.state.text}
+            />
         );
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         console.debug(`componentDidMount`);
         await this.loadFromNotion();
     }
 
     private async loadFromNotion() {
         console.debug(`Notepad.handleLoad: ${this.props.dateStr}`);
-        const token = localStorage.getItem('token')??'';
+        const token = localStorage.getItem('token') ?? '';
         const dbId = localStorage.getItem('dbId') ?? '';
         const getBlockIdTextResult = await Notepad.getBlockIdText(token, dbId, this.props.dateStr, this.props.dateStr);
-        const blockId = getBlockIdTextResult.blockId??'';
-        const text = getBlockIdTextResult.blockText??'';
+        const blockId = getBlockIdTextResult.blockId ?? '';
+        const text = getBlockIdTextResult.blockText ?? '';
         this.setState({
             blockId: blockId,
             text: text
@@ -46,40 +46,40 @@ export class Notepad extends React.Component<{dateStr: string}, {blockId: string
             isOk: boolean,
             blockId?: string,
             blockText?: string,
-        }>{
+        }> {
         const notion = new NotionHandler(token, dbId);
         const newTextTemplate = '';
 
         const getPageIdResult = await notion.getPageId(title, dateStr);
         console.debug(`getPageIdResult: ${JSON.stringify(getPageIdResult)}`);
-        if(!getPageIdResult.isOk){
-            return {isOk: false};
+        if (!getPageIdResult.isOk) {
+            return { isOk: false };
         }
         let pageId = getPageIdResult.pageId;
-        if(!getPageIdResult.pageId){
+        if (!getPageIdResult.pageId) {
             const createPageResult = await notion.createPage(title, dateStr);
             console.debug(`createPageResult: ${JSON.stringify(createPageResult)}`);
-            if(!createPageResult.isOk){
-                return {isOk: false};
+            if (!createPageResult.isOk) {
+                return { isOk: false };
             }
             pageId = createPageResult.pageId;
         }
-        if(!pageId){
-            return {isOk: false};
+        if (!pageId) {
+            return { isOk: false };
         }
 
         const getBlockIdTextResult = await notion.getBlockIdText(pageId);
         console.debug(`getBlockIdTextResult: ${JSON.stringify(getBlockIdTextResult)}`);
-        if(!getBlockIdTextResult.isOk){
-            return {isOk: false};
+        if (!getBlockIdTextResult.isOk) {
+            return { isOk: false };
         }
         let blockId = getBlockIdTextResult.id;
         let blockText = getBlockIdTextResult.text;
-        if(!getBlockIdTextResult.id){
+        if (!getBlockIdTextResult.id) {
             const createParagraphBlockResult = await notion.createParagraphBlock(pageId, newTextTemplate);
             console.debug(`createParagraphBlockResult: ${JSON.stringify(createParagraphBlockResult)}`);
-            if(!createParagraphBlockResult.isOk){
-                return {isOk: false};
+            if (!createParagraphBlockResult.isOk) {
+                return { isOk: false };
             }
             blockId = createParagraphBlockResult.blockId;
             blockText = newTextTemplate;
--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,6 @@ localStorage.setItem('dbId', dbId);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   //<React.StrictMode> //Commented out because `componentDidMount` is called twice if `React.StrictMode` is enabled.
-    <App />
+  <App />
   //</React.StrictMode>
 );
