abb72889520a1ba1d0c0aabcde4985b9db1d7849
hi_go <me@clutte.red>
2023-01-29 01:21:23 +0900

    Add React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import { App } from "./App";
 import "./style.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  //Commented out because `componentDidMount` is called twice if `React.StrictMode` is enabled.
-  //<React.StrictMode>
-  <App />
-  //</React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
